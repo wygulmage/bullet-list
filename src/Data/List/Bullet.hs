2b275e018e4e4757c25012c9f206194d22e5ef39
wygulmage <wygulmage@users.noreply.github.com>
2019-09-11 16:29:53 -0400

    added some list functions.

--- src/Data/List/Bullet.hs
+++ src/Data/List/Bullet.hs
@@ -1,15 +1,25 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE
+    UnicodeSyntax
+  , NoImplicitPrelude
+  , BangPatterns
+  , TypeFamilies #-}
 
 
 module Data.List.Bullet
    ( List (..)
    , (•)
+   , init, tail, unsnoc
    ) where
 
 
+import Prelude (Show, Bool (..), Maybe (..), (+), maybe, uncurry)
 import Control.Applicative (Applicative (pure, liftA2))
-import Data.Foldable (Foldable (foldl), foldr', foldl')
-import Data.Traversable (Traversable, fmapDefault, foldMapDefault)
+import Control.Category ((.))
+import Data.Semigroup
+import Data.Monoid
+import Data.Foldable (Foldable (fold, foldMap, foldl, length, null), foldr', foldl')
+import Data.Functor
+import Data.Traversable (Traversable (traverse), fmapDefault, foldMapDefault)
 import Data.Typeable
 import GHC.Exts (IsList(..))
 
@@ -38,14 +48,15 @@ infixl 1 •
 
 instance IsList (List a) where
    type Item (List a) = a
+
    toList = foldr' (:) []
+
    fromList = foldl' (:>) List
 
+
 instance Semigroup (List a) where
-   (<>) xs = loop
-      where
-      loop (ys :> y) = loop ys :> y
-      loop _ = xs
+   (<>) = foldl (:>)
+
 
 instance Monoid (List a) where
    mempty = List
@@ -53,18 +64,48 @@ instance Monoid (List a) where
 
 instance Functor List where
    fmap = fmapDefault
-   -- fmap f (xs :> x) = fmap f xs :> f x
-   -- fmap _ _ = List
+
 
 instance Foldable List where
+   fold = foldl (<>) mempty
+
+   foldMap f = foldl (\ x y -> x <> f y) mempty -- Compare to foldr ((<>) . f) mempty.
+
    foldl f z = loop
       where
       loop (xs :> x) = loop xs `f` x
       loop _ = z
-   foldMap f = foldl (flip (flip (<>) . f)) mempty
+
+   length =
+      foldr' (pure (+1)) 0
+
+   null List = True
+   null _ = False
+
 
 instance Traversable List where
    traverse f = loop
       where
       loop (xs :> x) = liftA2 (:>) (loop xs) (f x)
       loop _ = pure List
+
+
+
+----- Functions -----
+
+init :: List a -> List a
+init = foldl pure mempty
+-- init (xs :> _) = xs
+-- init _ = List
+
+tail :: List a -> List a
+
+tail = maybe mempty (uncurry loop) . unsnoc
+   where
+   loop (xs :> x') x = loop xs x' :> x
+   loop _ _ = List
+
+
+unsnoc :: List a -> Maybe (List a, a)
+unsnoc (xs :> x) = Just (xs, x)
+unsnoc _ = Nothing
